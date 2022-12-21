e82d9269b0be89fbce34c64bab1dce434c5119da
AnatoliyPetukh <k04375@gmail.com>
2022-12-21 22:39:06 +0100

    Initial commit

---
+++ Task1/src/Number7.java
@@ -0,0 +1,23 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Number7
+{
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("Please enter a number");
+        while (true){
+            number = input.nextInt();
+            if(number > 7) {
+                System.out.println("Привет");
+                break;
+            }
+            else {
+                System.out.println("Попробуйте еще раз");
+            }
+        }
+    }
+}
+
