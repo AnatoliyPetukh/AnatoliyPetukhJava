53a49046bbb2081a4191b1e3826a067fe53c995f
AnatoliyPetukh <k04375@gmail.com>
2022-12-21 23:05:15 +0100

    Дом задание

--- Task2/src/Rightname.java
+++ Task2/src/Rightname.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Rightname {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a your name");
+        while (true){
+            String name = input.nextLine();
+            if(name.equalsIgnoreCase("вячеслав")) {
+                System.out.println("Привет Вячеслав");
+                break;
+            }
+            else {
+                System.out.println("Нет такого имени");
+            }
+        }
     }
 }
--- Task3/src/Array.java
+++ Task3/src/Array.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Random;
+
+public class Array {
+
+    public static void main(String[] args){
+        System.out.println("Имеем массив:");
+        Random randI = new Random();
+        int[] myArray = new int[10];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = randI.nextInt(100);
+            System.out.print(myArray[i]+";");
+        }
+        System.out.println('\n' + "Числа кратные трем:");
+        for (int j : myArray) {
+            if (j % 3 == 0)
+                System.out.print(j + ";");
+        }
     }
+
 }
